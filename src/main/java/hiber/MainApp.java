de77b08cc800bc1a75368554ee9cdf1e152ab1d2
Ilya Glebov <glebovilya@bk.ru>
2024-07-29 15:29:56 +0300

    commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,53 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.persistence.NoResultException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+      Car car1 = new Car("BMW", 520);
+      Car car2 = new Car("Audi", 100);
+      Car car3 = new Car("VAZ", 2107);
+      Car car4 = new Car("GAZ", 24);
+
+      user1.setCar(car1).setUser(user1);
+      user2.setCar(car2).setUser(user2);
+      user3.setCar(car3).setUser(user3);
+      user4.setCar(car4).setUser(user4);
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println(user + " " + user.getCar());
+      }
+
+      try {
+         System.out.println(userService.getUserByCar("VAZ", 2107));
+      } catch (NoResultException e) {
+         System.out.println("Нет пользователя с такой машиной");
+      }
+
+      context.close();
+   }
+}
